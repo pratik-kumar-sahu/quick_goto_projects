1c7259c2e5e298d0bd0311be6b286bd77dee202b
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-05-09 17:53:45 +0530

    Added react-router-dom package and Home compoenent

---
+++ React simple website/src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home-container1">About Yourself</div>
+      <div className="home-container2">Your Experiences</div>
+    </div>
+  );
+}
+
+export default Home;
