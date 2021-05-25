083c543fa04ed7e59b924e5cf0c0367631acbcc5
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-05-25 18:54:43 +0530

    SignUp and login working using firebase

--- Custom login and localstorage/src/pages/Signup.js
+++ Custom login and localstorage/src/pages/Signup.js
@@ -4,22 +4,40 @@ import { auth } from "../firebase";
 function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if (email.trim() && password.trim()) {
+    if (email.trim() && password.trim() && name.trim()) {
       auth
         .createUserWithEmailAndPassword(email, password)
-        .then((res) => console.log(res.user))
+        .then((res) => {
+          if (res) {
+            res = auth.currentUser;
+            res.updateProfile({
+              displayName: name,
+              photoURL: `https://ui-avatars.com/api/?name=${name}`,
+            });
+          }
+          console.log(res);
+        })
         .catch((err) => console.log(err));
     }
     setEmail("");
     setPassword("");
+    setName("");
   };
 
   return (
     <div>
       <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter Name"
+          required
+        />
         <input
           type="email"
           value={email}
