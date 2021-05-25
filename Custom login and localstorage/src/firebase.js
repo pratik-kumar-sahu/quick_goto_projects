0663a0a3a70c2179d60eb6315e646b84e9b0f06d
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-05-25 17:51:04 +0530

    React initial setup with firebase

---
+++ Custom login and localstorage/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+import "firebase/auth";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC-pv-hUAOCAksRsqJdOHCw34R40neOuog",
+  authDomain: "quick-backend-dfafa.firebaseapp.com",
+  projectId: "quick-backend-dfafa",
+  storageBucket: "quick-backend-dfafa.appspot.com",
+  messagingSenderId: "923735613761",
+  appId: "1:923735613761:web:1c67d59c08f5b214a281c0",
+});
+
+const invokeFirestore = firebase.firestore();
+const auth = firebase.auth();
+
+export { invokeFirestore, auth };
