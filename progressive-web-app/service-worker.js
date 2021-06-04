c7737368bedd8d25636e8bed38a8bf2817ac1c4a
pratik-sahu-au13 <pratik.tcdurg@gmail.com>
2021-06-04 21:48:52 +0530

    strategies: network only, network with cache fallback , cache with network fallback

--- progressive-web-app/service-worker.js
+++ progressive-web-app/service-worker.js
@@ -1,4 +1,5 @@
 var CACHE_STATIC = "static";
+var CACHE_DYNAMIC = "dynamic";
 
 var urlsToCache = [
   "/",
@@ -22,6 +23,44 @@ self.addEventListener("activate", function (e) {
   console.log("SW activated");
 });
 
+// Cache with Network callback
 self.addEventListener("fetch", function (e) {
-  e.respondWith(caches.match(e.request));
+  e.respondWith(
+    caches.match(e.request).then(function (res) {
+      if (res) {
+        return res;
+      } else {
+        return fetch(e.request).then(function (res) {
+          return caches.open(CACHE_DYNAMIC).then(function (cache) {
+            cache.put(e.request.url, res.clone());
+            return res;
+          });
+        });
+      }
+    })
+  );
 });
+
+// Network with Cache Fallback
+// self.addEventListener("fetch", function (e) {
+//   e.respondWith(fetch(e.request))
+//     .then(function (res) {
+//       return caches.open(CACHE_DYNAMIC).then(function (cache) {
+//         cache.put(e.request.url, res.clone());
+//         return res;
+//       });
+//     })
+//     .catch(function () {
+//       return caches.match(e.request);
+//     });
+// });
+
+// Network only startegy
+// self.addEventListener("fetch", function (e) {
+//   e.respondWith(fetch(e.request));
+// });
+
+// Cache only startegy
+// self.addEventListener("fetch", function (e) {
+//   e.respondWith(caches.match(e.request));
+// });
